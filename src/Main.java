8c85094da66184ed4a56724bba788186decf24d2
MuFazaaa <muhammadfayyadhudzaqi@gmail.com>
2025-10-20 09:55:21 +0700

    sql2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
-
 public class Main {
     public static void main(String[] args) {
-    int a = 10;
-    int b = 5;
-    int sum = 10 +5;
-        System.out.println("Hasil penjumlahan : " + sum);
+        int a = 10;
+        int b = 5;
 
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
 
+        System.out.println("Bilangan pertama (a): " + a);
+        System.out.println("Bilangan kedua (b): " + b);
+        System.out.println("----------------------------------");
+        System.out.println("Hasil Penjumlahan (a + b): " + sum);
+        System.out.println("Hasil Pengurangan (a - b): " + difference);
+        System.out.println("Hasil Perkalian  (a * b): " + product);
     }
-
 }
