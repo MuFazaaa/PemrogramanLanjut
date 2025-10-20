61295825d8a1adcdf31e013cc5f555c7a2a598a9
MuFazaaa <muhammadfayyadhudzaqi@gmail.com>
2025-10-20 09:51:22 +0700

    sql

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 5;
+    int sum = 10 +5;
+        System.out.println("Hasil penjumlahan : " + sum);
+
+
+    }
+
+}
